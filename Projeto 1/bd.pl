1269315f6e67fa5132b835239845181ff750499c
Dong-Xuyong <68292731+Dong-Xuyong@users.noreply.github.com>
2022-03-28 19:11:49 +0100

    Add files via upload

---
+++ Projeto 1/bd.pl
@@ -0,0 +1,42 @@
+/*apartamento(nome, preco, perto_cidade, quartos, camas, wifi, cozinha, parque, maquina, casas_banho, jardim, piscina, estacionamento, score)*/
+apartamento('Encosta do paraiso', 125, 'nao', 2, 4, 'sim', 'sim', 'sim', 'sim', 2, 'sim', 'sim', 'nao', '8.6').
+apartamento('Anas Houses', 137, 'sim', 3, 3, 'sim', 'sim', 'sim', 'sim', 2, 'sim', 'nao', 'sim', '9.6').
+apartamento('Posh Residences', 79, 'sim', 2, 3, 'nao', 'sim', 'nao', 'nao', 1, 'nao', 'nao', 'nao', '8.4').
+apartamento('GuimaGold', 113, 'sim', 1, 2, 'sim', 'sim', 'nao', 'nao', 1, 'nao', 'nao', 'sim', '9.8').
+apartamento('Aparthotel Largo de Oliveira', 105, 'nao', 1, 1, 'sim', 'sim', 'nao', 'sim', 1, 'nao', 'nao', 'nao', '9').
+apartamento('Apartamentos com Historia', 138, 'nao' ,1, 1, 'sim', 'nao', 'nao', 'nao', 1, 'sim', 'nao', 'sim', '9.7').
+apartamento('Mezzazine Apartment', 99, 'nao', 1, 4, 'nao', 'nao', 'sim', 'sim', 1, 'sim', 'sim', 'nao', '9.6').
+apartamento('Casa Santa Luzia' 122, 'nao', 3, 3, 'sim', 'nao', 'sim', 'sim', 2, 'sim', 'sim', 'nao', '9.9').
+apartamento('TOISGA Apartment', 115, 'sim', 3, 4, 'sim', 'nao', 'sim', 'sim', 2, 'sim', 'sim', 'sim', '9.4').
+apartamento('Vila Baixa', 55, 'sim', 2, 2, 'sim', 'sim', 'sim', 'nao', 1, 'nao', 'sim', 'nao', '9.2').
+apartamento('Polery Apartments Center', 80, 'sim', 3, 3, 'sim', 'sim', 'nao', 'nao', 1, 'sim', 'nao', 'sim', '9.3').
+apartamento('Guimaraes Studios Lounge', 54, 'sim', 2, 3, 'nao', 'nao', 'nao', 'nao', 1, 'nao', 'nao', 'nao', '8.5').
+apartamento('The Office Guest', 97, 'sim', 2, 2, 'nao', 'sim', 'nao', 'nao', 1, 'nao', 'nao', 'sim', '9.3').
+apartamento('Casinha historica', 460, 'sim', 1, 1, 'nao', 'sim', 'nao', 'nao', 1, 'nao', 'nao', 'nao', '8').
+apartamento('Homely Large Flat- Balcony', 130, 'nao', 3, 4, 'sim', 'sim', 'nao', 'sim', 3, 'nao', 'nao', 'nao', '9.5').
+apartamento('Casa do Retiro', 100, 'sim', 2, 3, 'sim', 'sim', 'nao', 'sim', 1, 'nao', 'nao', 'sim', '7.5').
+apartamento('Santa Luzia', 80, 'nao', 2, 2, 'sim', 'nao', 'sim', 'sim', 1, 'nao', 'sim', 'nao', '9.3').
+apartamento('4-As center apartments', 59, 'sim', 2, 2, 'nao', 'sim', 'sim', 'sim', 1, 'sim', 'nao', 'nao', '8.6').
+apartamento('Old Market Place', 105, 'sim', 3, 3, 'sim', 'sim', 'sim', 'sim', 1, 'nao', 'sim', 'sim', '9.6').
+apartamento('C & H Studio', 55, 'nao', 1, 1, 'sim', 'sim', 'nao', 'nao', 1 , 'nao', 'sim', 'nao', '6.7').
+apartamento('Mumadona Apartment', 140, 'nao', 3, 6, 'sim', 'nao', 'nao', 'sim', 1 , 'nao', 'sim', 'sim', 9.4).
+apartamento('L & C Studio', 62, 'nao', 1, 1, 'nao', 'sim', 'sim', 'sim', 'sim', 1, 'nao', 'nao', 'nao', 9.2).
+apartamento('TM Guest House', 90, 'sim', 7, 13, 'nao', 'nao', 'nao', 'sim', 7, 'nao', 'sim', 'sim', '7.5').
+apartamento('Ferreiras House', 109, 'sim', 3, 6, 'nao', 'nao', 'nao', 'sim', 3, 'sim', 'nao', 'sim', '9.2').
+apartamento('Castle Apartment', 125, 'sim', 1, 4, 'sim', 'nao', 'nao', 'sim', 2, 'sim', 'nao', 'sim', '9.4').
+apartamento('Casa da Cera da Porta da Vila', 110, 'sim', 2, 2, 'sim', 'sim', 'sim', 'sim', 1, 'sim', 'nao', 'nao', '9.5').
+apartamento('Castelo Point', 165, 'nao', 2, 2, 'sim', 'sim', 'nao', 'sim', 2, 'nao', 'sim', 'sim', '9.2').
+apartamento('Molarinho Heritage', 80, 'nao', 2, 2, 'nao', 'sim', 'nao', 'nao', 2, 'nao', 'nao', 'nao', '9.3').
+apartamento('Old Market Apartment', 130, 'nao', 4, 6, 'sim', 'nao', 'sim', 'sim', 3, 'sim', 'nao', 'sim', '9.8').
+apartamento('Latelier Apartments', 155, 'sim', 1, 2, 'sim', 'nao', 'nao', 'nao', 2, 'sim', 'nao', 'nao', '9.6').
+apartamento('Casa da Calcada II', 140, 'nao', 2, 3, 'sim', 'nao', 'nao', 'sim', 1, 'sim', 'sim', 'nao', '9.3').
+apartamento('Casa Carla', 80, 'nao', 2, 2, 'nao', 'nao', 'sim', 'nao', 2, 'sim', 'nao', 'sim', '8.9').
+
+
+price(X,P1,P2,P):-
+    apartamento(X,P,_,_,_,_,_,_,_,_,_,_,_,_),
+    between(P1,P2,P).
+
+
+% between_(I,J,I) :- I =< J.
+% between_(I,J,K) :- I < J, I1 is I+1, between_(I1,J,K).
